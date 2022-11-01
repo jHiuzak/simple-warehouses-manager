60c2c5aeaf7c7220ffa2bb6d5db219febadb06de
Giovanni Vasconcelos <giovannivmedeiros@gmail.com>
2022-11-01 13:50:59 -0300

    Replace fillable for guarded property

--- app/Models/Warehouse.php
+++ app/Models/Warehouse.php
@@ -9,8 +9,15 @@ class Warehouse extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'warehouse',
-        'control_code'
+    // protected $fillable = [
+    //     'warehouse',
+    //     'control_code'
+    // ];
+
+    //You may specify what you want to make not fillable instead of the contrary
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at'
     ];
 }
