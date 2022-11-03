7d3371c8909a8bd9c12c4c7619b9a32b7ee3ae36
Giovanni Vasconcelos <giovannivmedeiros@gmail.com>
2022-11-03 11:03:00 -0300

    Configure Warehouse model

--- app/Models/Warehouse.php
+++ app/Models/Warehouse.php
@@ -9,15 +9,30 @@ class Warehouse extends Model
 {
     use HasFactory;
 
-    // protected $fillable = [
-    //     'warehouse',
-    //     'control_code'
-    // ];
+    protected $fillable = [
+        'warehouse',
+    ];
+
+    private const warehouse_prefix = 'x';
 
     //You may specify what you want to make not fillable instead of the contrary
-    protected $guarded = [
-        'id',
-        'created_at',
-        'updated_at'
-    ];
+    // protected $guarded = [
+    //     'id',
+    //     'created_at',
+    //     'updated_at'
+    // ];
+
+    // public function makeControlCode($id)
+    // {
+
+    // }
+
+    public function setControlCodeAttribute()
+    {
+        $control_code = $this->attributes['control_code'];
+        $id = $this->attributes['id'];
+        $warehouse_prefix = $this->warehouse_prefix;
+
+        $control_code = setControlCode($control_code, $id, $warehouse_prefix);
+    }
 }
